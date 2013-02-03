345e4082fff9a656041101b01ac5af9808e86e26
Aya <aya.dorghamy@gmail.com>
2013-02-03 11:12:28 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmApi::Application.config.secret_token = 'ada3598525f15731e1a56c2b471e35891c7a9b9a39f08506d4cd866e98546d2a3b0b8ab772d2ce01c367a1cfabdcd6ea4f9e75ad4df2ef287957da555999d8c8'
